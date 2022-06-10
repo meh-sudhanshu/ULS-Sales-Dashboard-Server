41fbd76e5ebcc81d7d4b097f9980357a45c0df28
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2022-06-10 10:28:55 +0530

    initial server

---
+++ ULS-Sales-DashBoard/src/main/java/com/ugb/ULS/Sales/DashBoard/UlsSalesDashBoardApplication.java
@@ -0,0 +1,13 @@
+package com.ugb.ULS.Sales.DashBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlsSalesDashBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UlsSalesDashBoardApplication.class, args);
+	}
+
+}
---
+++ ULS-Sales-DashBoard/src/test/java/com/ugb/ULS/Sales/DashBoard/UlsSalesDashBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugb.ULS.Sales.DashBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlsSalesDashBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
